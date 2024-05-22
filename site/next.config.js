8e0151e54ab75dd94d1ed05a23102093867e84c1
iLyax <ilyas.osmanoglu@gmail.com>
2024-05-22 12:11:29 +0300

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     name: 'Developtica',
     description: 'DPCA store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
+    icon: 'https://www.developtica.com/assets/images/res/dpca.png',
     listUrl: 'https://developtica.github.io/dpca-workspace/',
     contactUrl: 'https://github.com/developtica/dpca-workspace/issues',
   },
